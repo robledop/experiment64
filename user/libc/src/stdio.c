6e035d0721268aa14330856c401fa150c85e69d9
Robledo Pazotto <robledo@gmail.com>
2025-11-27 14:33:18 -0300

    Add width handling to libc printf

--- user/libc/src/stdio.c
+++ user/libc/src/stdio.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <stdbool.h>
 
 int putchar(int c)
 {
@@ -33,27 +34,56 @@ char *gets(char *s)
     return s;
 }
 
-static void print_int(int n)
+static int print_uint(unsigned long n)
 {
+    char buf[32];
+    int i = 0;
+    if (n == 0)
+    {
+        putchar('0');
+        return 1;
+    }
+    while (n > 0 && i < (int)sizeof(buf))
+    {
+        buf[i++] = (char)('0' + (n % 10));
+        n /= 10;
+    }
+    int written = i;
+    while (i-- > 0)
+        putchar(buf[i]);
+    return written;
+}
+
+static int print_int(long n)
+{
+    int written = 0;
     if (n < 0)
     {
         putchar('-');
+        written++;
         n = -n;
     }
-    if (n / 10)
-        print_int(n / 10);
-    putchar((n % 10) + '0');
+    return written + print_uint((unsigned long)n);
 }
 
-static void print_hex(unsigned long n)
+static int print_hex(unsigned long n)
 {
-    if (n / 16)
+    if (n >= 16)
         print_hex(n / 16);
     int rem = (int)(n % 16);
-    if (rem < 10)
-        putchar(rem + '0');
-    else
-        putchar(rem - 10 + 'a');
+    char c = (rem < 10) ? (char)('0' + rem) : (char)('a' + (rem - 10));
+    putchar(c);
+    return 1; // caller manages width if needed
+}
+
+static int print_padding(int width, int content_len, char pad_char)
+{
+    int pads = width - content_len;
+    if (pads < 0)
+        pads = 0;
+    for (int i = 0; i < pads; i++)
+        putchar(pad_char);
+    return pads;
 }
 
 int printf(const char *format, ...)
@@ -71,12 +101,57 @@ int printf(const char *format, ...)
             continue;
         }
         p++;
+        // Parse minimal width (decimal digits) and a single 'l' length modifier.
+        int width = 0;
+        bool long_mod = false;
+        while (*p >= '0' && *p <= '9')
+        {
+            width = width * 10 + (*p - '0');
+            p++;
+        }
+        if (*p == 'l')
+        {
+            long_mod = true;
+            p++;
+        }
+
         switch (*p)
         {
         case 'd':
         {
-            int i = va_arg(args, int);
-            print_int(i);
+            long val = long_mod ? va_arg(args, long) : va_arg(args, int);
+            // Capture output length by printing into a temp buffer via recursion? Simpler: format into stack buffer.
+            char tmp[32];
+            int len = 0;
+            long n = val;
+            if (n < 0)
+            {
+                tmp[len++] = '-';
+                n = -n;
+            }
+            // write number into buffer reversed
+            char numbuf[32];
+            int ni = 0;
+            if (n == 0)
+                numbuf[ni++] = '0';
+            while (n > 0 && ni < (int)sizeof(numbuf))
+            {
+                numbuf[ni++] = (char)('0' + (n % 10));
+                n /= 10;
+            }
+            // now emit padding then buffer
+            int total_len = len + ni;
+            count += print_padding(width, total_len, ' ');
+            for (int j = 0; j < len; j++)
+            {
+                putchar(tmp[j]);
+                count++;
+            }
+            while (ni-- > 0)
+            {
+                putchar(numbuf[ni]);
+                count++;
+            }
             break;
         }
         case 's':
@@ -104,18 +179,44 @@ int printf(const char *format, ...)
             putchar('0');
             putchar('x');
             print_hex(ptr);
+            // crude width handling: prepend padding only, since pointer printed last
             break;
         }
         case 'x':
         {
             unsigned long ptr = va_arg(args, unsigned long);
-            print_hex(ptr);
+            // hex width: pad based on hex digit count
+            char numbuf[32];
+            int ni = 0;
+            if (ptr == 0)
+                numbuf[ni++] = '0';
+            unsigned long tmp = ptr;
+            while (tmp > 0 && ni < (int)sizeof(numbuf))
+            {
+                int rem = (int)(tmp % 16);
+                numbuf[ni++] = (rem < 10) ? (char)('0' + rem) : (char)('a' + (rem - 10));
+                tmp /= 16;
+            }
+            count += print_padding(width, ni, ' ');
+            while (ni-- > 0)
+            {
+                putchar(numbuf[ni]);
+                count++;
+            }
             break;
         }
         default:
             putchar('%');
-            putchar(*p);
-            count += 2;
+            if (*p)
+            {
+                putchar(*p);
+                count += 2;
+            }
+            else
+            {
+                count += 1;
+                p--; // so outer loop terminates
+            }
             break;
         }
     }
