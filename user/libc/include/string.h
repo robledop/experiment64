20f3dc7f7141bb9bae43c8303b6822095fff9397
Robledo Pazotto <robledo@gmail.com>
2025-11-27 17:02:44 -0300

    libc: expand string helpers

--- user/libc/include/string.h
+++ user/libc/include/string.h
@@ -1,20 +1,23 @@
-#ifndef _STRING_H
-#define _STRING_H
+#pragma once
 
 #ifdef __clang__
 #pragma clang system_header
 #endif
 
 #include <stddef.h>
+#include <stdbool.h>
 
 size_t strlen(const char *s);
 int strcmp(const char *s1, const char *s2);
 int strncmp(const char *s1, const char *s2, size_t n);
 char *strcpy(char *dest, const char *src);
+char *strncpy(char *dst, const char *src, size_t n);
 void *memset(void *s, int c, size_t n);
 void *memcpy(void *dest, const void *src, size_t n);
 void *memmove(void *dest, const void *src, size_t n);
 char *strtok(char *str, const char *delim);
 char *strchr(const char *s, int c);
-
-#endif
+bool starts_with(const char pre[static 1], const char str[static 1]);
+char *strcat(char dest[static 1], const char src[static 1]);
+char *strncat(char dest[static 1], const char src[static 1], size_t n);
+void reverse(char *s);
--- user/libc/src/string.c
+++ user/libc/src/string.c
@@ -1,6 +1,6 @@
 #include <string.h>
 
-size_t strlen(const char *s)
+size_t strlen(const char* s)
 {
     size_t len = 0;
     while (s[len])
@@ -8,17 +8,17 @@ size_t strlen(const char *s)
     return len;
 }
 
-int strcmp(const char *s1, const char *s2)
+int strcmp(const char* s1, const char* s2)
 {
     while (*s1 && (*s1 == *s2))
     {
         s1++;
         s2++;
     }
-    return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
-int strncmp(const char *s1, const char *s2, size_t n)
+int strncmp(const char* s1, const char* s2, size_t n)
 {
     while (n > 0 && *s1 && (*s1 == *s2))
     {
@@ -28,38 +28,52 @@ int strncmp(const char *s1, const char *s2, size_t n)
     }
     if (n == 0)
         return 0;
-    return *(const unsigned char *)s1 - *(const unsigned char *)s2;
+    return *(const unsigned char*)s1 - *(const unsigned char*)s2;
 }
 
-char *strcpy(char *dest, const char *src)
+char* strcpy(char* dest, const char* src)
 {
-    char *d = dest;
-    while ((*d++ = *src++))
-        ;
+    char* d = dest;
+    while ((*d++ = *src++));
     return dest;
 }
 
-void *memset(void *s, int c, size_t n)
+char *strncpy(char *dst, const char *src, size_t n)
 {
-    unsigned char *p = s;
+    size_t i = 0;
+
+    for (; i < n && src[i] != '\0'; i++) {
+        dst[i] = src[i];
+    }
+
+    for (; i < n; i++) {
+        dst[i] = '\0';
+    }
+
+    return dst;
+}
+
+void* memset(void* s, int c, size_t n)
+{
+    unsigned char* p = s;
     while (n--)
         *p++ = (unsigned char)c;
     return s;
 }
 
-void *memcpy(void *dest, const void *src, size_t n)
+void* memcpy(void* dest, const void* src, size_t n)
 {
-    char *d = dest;
-    const char *s = src;
+    char* d = dest;
+    const char* s = src;
     while (n--)
         *d++ = *s++;
     return dest;
 }
 
-void *memmove(void *dest, const void *src, size_t n)
+void* memmove(void* dest, const void* src, size_t n)
 {
-    char *d = (char *)dest;
-    const char *s = (const char *)src;
+    char* d = (char*)dest;
+    const char* s = (const char*)src;
     if (d == s || n == 0)
         return dest;
 
@@ -78,7 +92,7 @@ void *memmove(void *dest, const void *src, size_t n)
     return dest;
 }
 
-char *strchr(const char *s, int c)
+char* strchr(const char* s, int c)
 {
     while (*s != (char)c)
     {
@@ -87,12 +101,12 @@ char *strchr(const char *s, int c)
             return nullptr;
         }
     }
-    return (char *)s;
+    return (char*)s;
 }
 
-char *strtok(char *str, const char *delim)
+char* strtok(char* str, const char* delim)
 {
-    static char *next_token = nullptr;
+    static char* next_token = nullptr;
     if (str)
         next_token = str;
     if (!next_token)
@@ -109,7 +123,7 @@ char *strtok(char *str, const char *delim)
     if (!*next_token)
         return nullptr;
 
-    char *start = next_token;
+    char* start = next_token;
     while (*next_token)
     {
         if (strchr(delim, *next_token))
@@ -121,4 +135,55 @@ char *strtok(char *str, const char *delim)
         next_token++;
     }
     return start;
+}
+
+bool starts_with(const char pre[static 1], const char str[static 1])
+{
+    return strncmp(pre, str, strlen(pre)) == 0;
+}
+
+char* strcat(char dest[static 1], const char src[static 1])
+{
+    char* d = dest;
+    const char* s = src;
+    while (*d != '\0')
+    {
+        d++;
+    }
+    while (*s != '\0')
+    {
+        *d++ = *s++;
+    }
+    *d = '\0';
+    return dest;
+}
+
+char* strncat(char dest[static 1], const char src[static 1], size_t n)
+{
+    char* d = dest;
+    const char* s = src;
+    size_t copied = 0;
+    while (*d != '\0')
+    {
+        d++;
+    }
+    while (copied < n && *s != '\0')
+    {
+        *d++ = *s++;
+        copied++;
+    }
+    *d = '\0';
+    return dest;
+}
+
+void reverse(char* s)
+{
+    int i, j;
+
+    for (i = 0, j = (int)strlen(s) - 1; i < j; i++, j--)
+    {
+        const int c = s[i];
+        s[i] = s[j];
+        s[j] = c;
+    }
 }
