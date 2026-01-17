b7ceb1130faaa80ae49d3bf76b7614b2dbd9adcf
Robledo Pazotto <robledo@gmail.com>
2026-01-17 14:55:08 -0300

    Add generic `defer` mechanism and cleanup utility function

--- user/libc/include/util.h
+++ user/libc/include/util.h
@@ -16,3 +16,34 @@ static inline int clamp_signed_to_int(int64_t value)
         return INT_MIN;
     return (int)value;
 }
+
+typedef void (*defer_func_t)(void *);
+
+struct defer_action
+{
+    defer_func_t func;
+    void *arg;
+};
+
+static inline void defer_cleanup(struct defer_action *action)
+{
+    if (action && action->func)
+        action->func(action->arg);
+}
+
+#define DEFER_NAME(base, line) base##line
+#define DEFER(base, line) DEFER_NAME(base, line)
+#define defer(func, arg) __attribute__((cleanup(defer_cleanup))) struct defer_action DEFER(_defer_, __LINE__) = {func, arg}
+
+static inline void cleanup_free(void *ptr)
+{
+    if (!ptr)
+    {
+        return;
+    }
+    auto p = (void **)ptr;
+    if (*p)
+    {
+        free(*p);
+    }
+}
