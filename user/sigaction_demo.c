30e668b2bf1d0cf1dd615fb8afffeb529b5d8323
Robledo Pazotto <robledo@gmail.com>
2026-01-20 16:31:32 -0300

    Add `sigaction_demo.c` example for signal handling demonstration

---
+++ user/sigaction_demo.c
@@ -0,0 +1,40 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static volatile int got_signal = 0;
+static volatile int last_signal = 0;
+
+static void sig_handler(const int sig)
+{
+    last_signal = sig;
+    got_signal = 1;
+}
+
+int main(void)
+{
+    struct sigaction sa = {0};
+    sa.sa_handler = sig_handler;
+
+    if (sigaction(SIGTERM, &sa, nullptr) < 0)
+    {
+        printf("sigaction(SIGTERM) failed\n");
+        return 1;
+    }
+    if (sigaction(SIGUSR1, &sa, nullptr) < 0)
+    {
+        printf("sigaction(SIGUSR1) failed\n");
+        return 1;
+    }
+
+    printf("sigaction_demo: pid=%d\n", getpid());
+    printf("Send SIGTERM (kill <pid>) or SIGUSR1 (kill -10 <pid>)\n");
+
+    while (!got_signal)
+    {
+        usleep(200000);
+    }
+
+    printf("sigaction_demo: received signal %d, exiting.\n", last_signal);
+    return 0;
+}
