6261a75d285f6db4330da760041e86d825010159
Robledo Pazotto <robledo@gmail.com>
2026-01-20 15:01:36 -0300

    Add support for sending specific signals via `kill` command and streamline `sys_kill` implementation

--- user/kill.c
+++ user/kill.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,12 +6,26 @@ int main(int argc, char** argv)
 {
     if (argc < 2)
     {
-        printf("usage: kill pid...\n");
+        printf("usage: kill [-SIGNAL] pid...\n");
         exit();
     }
-    for (int i = 1; i < argc; i++)
+
+    int sig = SIGTERM;
+    int first_pid = 1;
+    if (argv[1][0] == '-')
     {
-        kill((int)strtol(argv[i], nullptr, 10), 0);
+        sig = (int)strtol(argv[1] + 1, nullptr, 10);
+        if (sig <= 0 || argc < 3)
+        {
+            printf("usage: kill [-SIGNAL] pid...\n");
+            exit();
+        }
+        first_pid = 2;
+    }
+
+    for (int i = first_pid; i < argc; i++)
+    {
+        kill((int)strtol(argv[i], nullptr, 10), sig);
     }
 
     return 0;
