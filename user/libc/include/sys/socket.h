3517a41ef95a22a6939f910281555f9c5af899c9
Robledo Pazotto <robledo@gmail.com>
2026-01-20 11:08:40 -0300

    Add `send` syscall wrapper function around `sendto`

--- user/libc/include/sys/socket.h
+++ user/libc/include/sys/socket.h
@@ -41,6 +41,7 @@ int socket(int domain, int type, int protocol);
 int bind(int sockfd, const struct sockaddr *addr, size_t addrlen);
 int listen(int sockfd, int backlog);
 int accept(int sockfd, struct sockaddr *addr, socklen_t addrlen);
+ssize_t send(int sockfd, const void *buf, size_t len, int flags);
 ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
                const struct sockaddr *dest_addr, socklen_t addrlen);
 ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
