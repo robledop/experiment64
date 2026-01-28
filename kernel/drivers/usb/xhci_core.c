c8ba9fb0a28525c39e7aa4516a7dbcb87ad31cc2
Robledo Pazotto <robledo@gmail.com>
2026-01-28 12:58:19 -0300

    Refactoring

--- kernel/drivers/usb/xhci_core.c
+++ kernel/drivers/usb/xhci_core.c
@@ -74,60 +74,6 @@ static struct xhci_device *xhci_device_from_slot(const uint8_t slot_id)
     return &g_xhci_devices[slot_id];
 }
 
-static inline pml4_t xhci_current_pml4(void)
-{
-    uint64_t cr3 = 0;
-    __asm__ volatile("mov %0, cr3" : "=r"(cr3));
-    return (pml4_t)(cr3 & 0x000FFFFFFFFFF000ull);
-}
-
-static bool xhci_virt_mapped(const uint64_t *pml4, const uint64_t virt)
-{
-    auto pml4_virt        = (const uint64_t *)((uint64_t)pml4 + g_hhdm_offset);
-    const size_t pml4_idx = (virt >> 39) & 0x1FFu;
-    const uint64_t pml4e  = pml4_virt[pml4_idx];
-    if ((pml4e & PTE_PRESENT) == 0) {
-        return false;
-    }
-
-    auto pdpt             = (const uint64_t *)((pml4e & 0x000FFFFFFFFFF000ull) + g_hhdm_offset);
-    const size_t pdpt_idx = (virt >> 30) & 0x1FFu;
-    const uint64_t pdpte  = pdpt[pdpt_idx];
-    if ((pdpte & PTE_PRESENT) == 0) {
-        return false;
-    }
-    if ((pdpte & PTE_HUGE) != 0) {
-        return true;
-    }
-
-    auto pd             = (const uint64_t *)((pdpte & 0x000FFFFFFFFFF000ull) + g_hhdm_offset);
-    const size_t pd_idx = (virt >> 21) & 0x1FFu;
-    const uint64_t pde  = pd[pd_idx];
-    if ((pde & PTE_PRESENT) == 0) {
-        return false;
-    }
-    if ((pde & PTE_HUGE) != 0) {
-        return true;
-    }
-
-    auto pt             = (const uint64_t *)((pde & 0x000FFFFFFFFFF000ull) + g_hhdm_offset);
-    const size_t pt_idx = (virt >> 12) & 0x1FFu;
-    return (pt[pt_idx] & PTE_PRESENT) != 0;
-}
-
-static void xhci_map_mmio_range(const uint64_t phys_base, const uint64_t bytes)
-{
-    const uint64_t start = phys_base & ~(uint64_t)(PAGE_SIZE - 1u);
-    const uint64_t end   = (phys_base + bytes + PAGE_SIZE - 1u) & ~(uint64_t)(PAGE_SIZE - 1u);
-    pml4_t pml4          = xhci_current_pml4();
-    for (uint64_t phys = start; phys < end; phys += PAGE_SIZE) {
-        const uint64_t virt = phys + g_hhdm_offset;
-        if (!xhci_virt_mapped(pml4, virt)) {
-            vmm_map_page(pml4, virt, phys, PTE_PRESENT | PTE_WRITABLE | PTE_PCD | PTE_PWT);
-        }
-    }
-}
-
 static bool xhci_get_mmio_bar(const struct pci_device *device, uint64_t *base_out)
 {
     const uint32_t bar0 = device->bars[0];
