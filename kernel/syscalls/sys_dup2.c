99a406301232479bd309a211fe41ea791f1f01d0
Robledo Pazotto <robledo@gmail.com>
2026-02-18 19:56:12 -0300

    Implemented dup2 syscall

---
+++ kernel/syscalls/sys_dup2.c
@@ -0,0 +1,34 @@
+#include <status.h>
+#include <sys/syscall.h>
+#include <syscall_common.h>
+
+/**
+ * Duplicate a file descriptor to a specific new fd, closing the new fd first if it is already open. If oldfd and newfd
+ * are the same, dup2 does nothing and returns newfd. The new file descriptor shares the same underlying file
+ * description as oldfd, and thus the same file offset and file status flags.
+ * @param oldfd The file descriptor to duplicate.
+ * @param newfd The target file descriptor.
+ * @return The new file descriptor on success, or a negative error code on failure.
+ */
+int sys_dup2(int oldfd, int newfd)
+{
+    if (oldfd == newfd)
+        return newfd;
+    if (newfd < 0 || newfd >= MAX_FDS)
+        return -EBADF;
+
+    file_descriptor_t *old_desc = fd_get(oldfd);
+    if (!old_desc)
+        return -EBADF;
+
+    sys_close(newfd);
+
+    uint64_t flags;
+    SPIN_LOCK_INT_SAVE(current_process->fd_lock, flags);
+    current_process->fd_table[newfd] = old_desc;
+    __atomic_add_fetch(&old_desc->ref, 1, __ATOMIC_RELAXED);
+    SPIN_UNLOCK_INT_RESTORE(current_process->fd_lock, flags);
+
+    fd_put(old_desc);
+    return newfd;
+}
